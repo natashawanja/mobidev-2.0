14f9a6239a8fe0ce7ca8b3f96f512263bce6749b
natashawanja <kinuthianatasha@gmail.com>
2023-07-12 18:12:27 +0300

    update

--- js/main.js
+++ js/main.js
@@ -136,5 +136,25 @@ jQuery(document).ready(function($) {
                     dfIcon.style.bottom = "60px";
                 }
             });
+  document.addEventListener('DOMContentLoaded', function() {
+  const slides = document.querySelectorAll('.slide');
+  let currentSlide = 0;
+
+  function showSlide(slideIndex) {
+    slides.forEach(function(slide) {
+      slide.classList.remove('active');
+    });
+
+    slides[slideIndex].classList.add('active');
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  setInterval(nextSlide, 3000); // Change slide every 3 seconds
+});
+
 
 });
